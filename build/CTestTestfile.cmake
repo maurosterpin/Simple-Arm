678a074dcd33b810f139c8480d914a818ca08a9a
maurosterpin <msterpindev@gmail.com>
2022-11-19 21:13:40 +0100

    arm mover node

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mauro/catkin_ws/src
-# Build directory: /home/mauro/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("first_package")
-subdirs("simple_arm")
